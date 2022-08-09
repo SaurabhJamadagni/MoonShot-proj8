3567c1aef43d51e2fa7c1ab8792ad214f41421a5
Saurabh Jamadagni <saurabhjamadagni2002@gmail.com>
2022-08-09 17:09:48 +0530

    initial commit

---
+++ MoonShot2/MoonShot2App.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShot2App.swift
+//  MoonShot2
+//
+//  Created by Admin on 09/08/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonShot2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
